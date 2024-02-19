0e9a1f61cbfabdbd75ca94ff7f7779216cf828a7
Raiane <raiane.oliveira404@gmail.com>
2024-02-19 12:10:22 -0300

    fix: add missing env variables to env schema

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config'
-import { z } from "zod";
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
-  JWT_SECRET: z.string()
+  JWT_SECRET: z.string(),
+  DATABASE_URL: z.string().url(),
 })
 
 const _env = envSchema.safeParse(process.env)
